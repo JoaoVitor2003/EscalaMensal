1ae0b3b7031b5a7d8cf627c43ae751aba8e473d8
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-05-26 21:46:12 -0300

    feat: Adicionando a entidade usuário.

---
+++ EscalaMensal.Domain/Entities/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Entities
+{
+    namespace EscalaMensal.Domain.Entities
+    {
+        public class Usuario
+        {
+            public int Id { get; private set; }
+            public string Nome { get; private set; }
+            public string Email { get; private set; }
+            public bool Ativo { get; private set; }
+
+            public int NivelId { get; private set; }
+
+            public int? UsuarioVinculadoId { get; private set; }
+            public Usuario? UsuarioVinculado { get; private set; }
+
+            public Usuario(string nome, string email, int nivelId)
+            {
+                Nome = nome;
+                Email = email;
+                NivelId = nivelId;
+                Ativo = true;
+            }
+
+            public void VincularUsuario(int usuarioId) => UsuarioVinculadoId = usuarioId;
+            public void DesvincularUsuario() => UsuarioVinculadoId = null;
+
+            public void Ativar() => Ativo = true;
+            public void Desativar() => Ativo = false;
+        }
+
+    }
+
+}
