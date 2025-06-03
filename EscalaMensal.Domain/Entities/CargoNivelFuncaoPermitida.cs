a23f764fe48a2774e2a4baf529ae4e08b7a8d780
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-06-02 22:16:23 -0300

    feat: Adicionando a classe Cargo Nivel Função Permitida.

---
+++ EscalaMensal.Domain/Entities/CargoNivelFuncaoPermitida.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Entities
+{
+    public class CargoNivelFuncaoPermitida
+    {
+        public int Id { get; private set; }
+
+        public int CargoId { get; private set; }
+        public Cargo Cargo { get; private set; }
+
+        public int NivelId { get; private set; }
+        public Nivel Nivel { get; private set; }
+
+        public int FuncaoId { get; private set; }
+        public Funcao Funcao { get; private set; }
+
+        public CargoNivelFuncaoPermitida(int cargoId, int nivelId, int funcaoId)
+        {
+            CargoId = cargoId;
+            NivelId = nivelId;
+            FuncaoId = funcaoId;
+        }
+    }
+
+}
