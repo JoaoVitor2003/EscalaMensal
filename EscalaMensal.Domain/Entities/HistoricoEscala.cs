969a64fc4731b85c36825aeda6cf434595466470
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-06-03 23:40:51 -0300

    feat: Adicionando o histórico da escala para facilitar não criar escalas iguais.

---
+++ EscalaMensal.Domain/Entities/HistoricoEscala.cs
@@ -0,0 +1,31 @@
+﻿using EscalaMensal.Domain.Entities.EscalaMensal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Entities
+{
+    public class HistoricoEscala
+    {
+        public int Id { get; private set; }
+
+        public DateTime Data { get; private set; }
+
+        public int UsuarioId { get; private set; }
+        public Usuario Usuario { get; private set; }
+
+        public int FuncaoId { get; private set; }
+        public Funcao Funcao { get; private set; }
+
+
+        public HistoricoEscala(int usuarioId, int funcaoId, DateTime data)
+        {
+            UsuarioId = usuarioId;
+            FuncaoId = funcaoId;
+            Data = data;
+        }
+    }
+
+}
