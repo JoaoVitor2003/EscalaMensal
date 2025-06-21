38ee499adaba3b82e4d03fddf61c4b244516b828
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-06-20 21:40:36 -0300

    feat: Criando os enums que serão necessários.

---
+++ EscalaMensal.Domain/Enums/CargoEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Enums
+{
+    public enum CargoEnum
+    {
+        Coroinha = 1,
+        Cerimoniario = 2
+    }
+}
---
+++ EscalaMensal.Domain/Enums/NivelEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Enums
+{
+    public enum NivelEnum
+    {
+        Nivel1 = 1,
+        Nivel2 = 2,
+        Nivel3 = 3
+    }
+}
