5b0b07ec8ed421d31f0072f2cb7fd5e147083c1b
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-06-29 23:33:17 -0300

    feat: Implementando a primeira interface.

---
+++ EscalaMensal.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using EscalaMensal.Domain.Entities.EscalaMensal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario?> ObterPorIdAsync(int id);
+        Task<List<Usuario>> ObterTodosAsync();
+        Task AdicionarAsync(Usuario usuario);
+        Task AtualizarAsync(Usuario usuario);
+        Task RemoverAsync(int id);
+    }
+
+}
