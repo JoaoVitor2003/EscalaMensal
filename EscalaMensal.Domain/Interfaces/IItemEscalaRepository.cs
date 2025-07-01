566fb8a6e568efc49dc8e333a18bbdf92ccabbbb
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-06-30 23:38:46 -0300

    feat: Incluindo a interface do ItemEscala

---
+++ EscalaMensal.Domain/Interfaces/IItemEscalaRepository.cs
@@ -0,0 +1,18 @@
+﻿using EscalaMensal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Interfaces
+{
+    public interface IItemEscalaRepository
+    {
+        Task<List<ItemEscala>> ObterPorEscalaIdAsync(int escalaId);
+        Task AdicionarAsync(ItemEscala item);
+        Task AtualizarAsync(ItemEscala item);
+        Task RemoverAsync(int id);
+    }
+
+}
