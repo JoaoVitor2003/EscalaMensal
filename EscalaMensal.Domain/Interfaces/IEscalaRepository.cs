6532dc88829d658e459d802efaa03f41909cf934
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-06-30 23:36:35 -0300

    feat: Subindo a interface da escala.

---
+++ EscalaMensal.Domain/Interfaces/IEscalaRepository.cs
@@ -0,0 +1,19 @@
+﻿using EscalaMensal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Interfaces
+{
+    public interface IEscalaRepository
+    {
+        Task<Escala?> ObterPorMesAnoAsync(int mes, int ano);
+        Task<List<Escala>> ObterTodasAsync();
+        Task AdicionarAsync(Escala escala);
+        Task AtualizarAsync(Escala escala);
+        Task RemoverAsync(int id);
+    }
+
+}
