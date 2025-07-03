31b972fe6060cec93d9373359c2a5c88d80cf202
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-07-02 22:38:27 -0300

    feat: Criando a interface da Função repository

---
+++ EscalaMensal.Domain/Interfaces/IFuncaoRepository.cs
@@ -0,0 +1,18 @@
+﻿using EscalaMensal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Interfaces
+{
+    public interface IFuncaoRepository
+    {
+        Task<List<Funcao>> ObterTodasAsync();
+        Task<Funcao?> ObterPorIdAsync(int id);
+        Task AdicionarAsync(Funcao funcao);
+        Task AtualizarAsync(Funcao funcao);
+        Task RemoverAsync(int id);
+    }
+}
