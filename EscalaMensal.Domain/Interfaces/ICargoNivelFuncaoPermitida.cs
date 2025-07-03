05586378a5bcd4dcbbbaae4835f72bdd33c5ff0b
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-07-02 22:43:24 -0300

    feat: Adicionando a interface do cargo nivel funçao permitida.

---
+++ EscalaMensal.Domain/Interfaces/ICargoNivelFuncaoPermitida.cs
@@ -0,0 +1,16 @@
+﻿using EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Interfaces
+{
+    public interface ICargoNivelFuncaoPermitidaRepository
+    {
+        Task<List<Funcao>> ObterFuncoesPermitidasAsync(CargoEnum cargo, NivelEnum nivel);
+        Task<List<CargoNivelFuncaoPermitida>> ObterTodasAsync();
+    }
+}
