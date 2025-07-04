d2cd5c51b51f201d2aeb380c037d6b17ea012bcf
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-07-04 20:14:37 -0300

    feat: Adicionando a interface de restrição.

---
+++ EscalaMensal.Domain/Interfaces/IRestricaoRepository.cs
@@ -0,0 +1,18 @@
+﻿using EscalaMensal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Interfaces
+{
+    public interface IRestricaoRepository
+    {
+        Task<List<Restricao>> ObterPorMesAnoAsync(int mes, int ano);
+        Task<List<Restricao>> ObterPorUsuarioIdAsync(int usuarioId, int mes, int ano);
+        Task AdicionarAsync(Restricao restricao);
+        Task AtualizarAsync(Restricao restricao);
+        Task RemoverAsync(int id);
+    }
+}
