abbc0dc6b7cda0e6b43f9d5b1bb46655f5fe8069
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-07-07 22:44:35 -0300

    feat: Adicionando a interface do histórico.

---
+++ EscalaMensal.Domain/Interfaces/IHistoricoEscalaRepository.cs
@@ -0,0 +1,17 @@
+﻿using EscalaMensal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Domain.Interfaces
+{
+    public interface IHistoricoEscalaRepository
+    {
+        Task<List<HistoricoEscala>> ObterPorMesAnoAsync(int mes, int ano);
+        Task<List<HistoricoEscala>> ObterPorUsuarioIdAsync(int usuarioId);
+        Task AdicionarAsync(HistoricoEscala historico);
+    }
+
+}
