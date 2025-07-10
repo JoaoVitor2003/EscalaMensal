6422bc3b23536f5e3174490590251af97b66942e
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-07-09 23:41:18 -0300

    feat: Implementando a interface do Usuario

---
+++ EscalaMensal.Infrastructure/Repositories/UsuarioRepository.cs
@@ -0,0 +1,52 @@
+﻿using EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Entities.EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Interfaces;
+using EscalaMensal.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscalaMensal.Infrastructure.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UsuarioRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AdicionarAsync(Usuario usuario)
+        {
+            await _context.Usuarios.AddAsync(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AtualizarAsync(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Usuario?> ObterPorIdAsync(int id)
+        {
+            return await _context.Usuarios
+                .Include(u => u.UsuarioVinculado)
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<List<Usuario>> ObterTodosAsync()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        public async Task RemoverAsync(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
