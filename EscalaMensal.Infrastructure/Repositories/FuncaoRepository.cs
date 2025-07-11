6c7b65a32bdc7005a809ec36f1614edb5257d3d3
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-07-10 23:38:28 -0300

    feat: Implementando o repositório da função.

---
+++ EscalaMensal.Infrastructure/Repositories/FuncaoRepository.cs
@@ -0,0 +1,49 @@
+﻿using EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Interfaces;
+using EscalaMensal.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscalaMensal.Infrastructure.Repositories
+{
+    public class FuncaoRepository : IFuncaoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public FuncaoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Funcao>> ObterTodasAsync()
+        {
+            return await _context.Funcoes.ToListAsync();
+        }
+
+        public async Task<Funcao?> ObterPorIdAsync(int id)
+        {
+            return await _context.Funcoes.FindAsync(id);
+        }
+
+        public async Task AdicionarAsync(Funcao funcao)
+        {
+            await _context.Funcoes.AddAsync(funcao);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AtualizarAsync(Funcao funcao)
+        {
+            _context.Funcoes.Update(funcao);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoverAsync(int id)
+        {
+            var funcao = await _context.Funcoes.FindAsync(id);
+            if (funcao != null)
+            {
+                _context.Funcoes.Remove(funcao);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
