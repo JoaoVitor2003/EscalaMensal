68a9f517ba2f3721d81db8f6865af1facfaf1a79
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-07-28 18:15:20 -0300

    feat: Adicionando o controlador de usuario

---
+++ EscalaMensal.API/Controllers/UsuarioController.cs
@@ -0,0 +1,60 @@
+﻿using EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Entities.EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscalaMensal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Usuario>>> ObterTodos()
+        {
+            var usuarios = await _usuarioService.ObterTodosAsync();
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> ObterPorId(int id)
+        {
+            var usuario = await _usuarioService.ObterPorIdAsync(id);
+            if (usuario == null)
+                return NotFound();
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Adicionar([FromBody] Usuario usuario)
+        {
+            await _usuarioService.AdicionarAsync(usuario);
+            return CreatedAtAction(nameof(ObterPorId), new { id = usuario.Id }, usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Atualizar(int id, [FromBody] Usuario usuario)
+        {
+            if (id != usuario.Id)
+                return BadRequest("IDs não coincidem");
+
+            await _usuarioService.AtualizarAsync(usuario);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Remover(int id)
+        {
+            await _usuarioService.RemoverAsync(id);
+            return NoContent();
+        }
+    }
+}
