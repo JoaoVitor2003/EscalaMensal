f1574818472e39ccdac863a352b1479f151ea16b
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-07-30 21:54:31 -0300

    feat: Adicionando controller para Cargo Nivel Função Permitida

---
+++ EscalaMensal.API/Controllers/CargoNivelFuncaoPermitidaController.cs
@@ -0,0 +1,33 @@
+﻿using EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Enums;
+using EscalaMensal.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscalaMensal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CargoNivelFuncaoPermitidaController : ControllerBase
+    {
+        private readonly ICargoNivelFuncaoPermitidaRepository _repository;
+
+        public CargoNivelFuncaoPermitidaController(ICargoNivelFuncaoPermitidaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("funcoes")]
+        public async Task<IActionResult> ObterFuncoesPermitidas([FromQuery] CargoEnum cargo, [FromQuery] NivelEnum nivel)
+        {
+            var funcoes = await _repository.ObterFuncoesPermitidasAsync(cargo, nivel);
+            return Ok(funcoes);
+        }
+
+        [HttpGet("todas")]
+        public async Task<IActionResult> ObterTodas()
+        {
+            var todas = await _repository.ObterTodasAsync();
+            return Ok(todas);
+        }
+    }
+}
