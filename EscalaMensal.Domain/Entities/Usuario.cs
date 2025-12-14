ebded48dcead79608238ff84f3c8c7ecfd97241f
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-12-13 23:42:06 -0300

    refact: Adicionando funcionalidade para o usuário

--- EscalaMensal.Domain/Entities/Usuario.cs
+++ EscalaMensal.Domain/Entities/Usuario.cs
@@ -21,6 +21,7 @@ namespace EscalaMensal.Domain.Entities
         public bool DisponivelQuinta { get; set; }
         public int? UsuarioVinculadoId { get; private set; }
         public Usuario? UsuarioVinculado { get; private set; }
+        public int? DiasEscalados { get; set; }
         public ICollection<Restricao> Restricoes { get; private set; } = new List<Restricao>();
         public Usuario(string nome, NivelEnum nivel, CargoEnum cargo, int? usuarioVinculadoId = null)
         {
