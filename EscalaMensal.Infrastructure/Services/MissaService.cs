fc50975fb05ed736819b39f0a69af1495123a9a0
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-12-15 22:38:06 -0300

    feat: Implementando os serviços de Missa

---
+++ EscalaMensal.Infrastructure/Services/MissaService.cs
@@ -0,0 +1,39 @@
+﻿using EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Interfaces;
+using EscalaMensal.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Infrastructure.Services
+{
+    public class MissaService : IMissaService
+    {
+        private readonly MissasRepository _missaRepository;
+        public MissaService(MissasRepository missaRepository)
+        {
+            _missaRepository = missaRepository;
+        }
+        public async Task AdicionarAsync(Missas missa)
+        {
+            await _missaRepository.AdicionarAsync(missa);
+        }
+
+        public async Task AtualizarAsync(Missas missa)
+        {
+            await _missaRepository.AtualizarAsync(missa);
+        }
+
+        public async Task<List<Missas>> ObterPorEscalaIdAsync(int escalaId)
+        {
+            return await _missaRepository.ObterPorEscalaIdAsync(escalaId);
+        }
+
+        public async Task RemoverAsync(int id)
+        {
+            await _missaRepository.RemoverAsync(id);
+        }
+    }
+}
