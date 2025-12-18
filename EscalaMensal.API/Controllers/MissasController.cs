041ffc7d69ed6ee8196351546e5add4db7fc89dd
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-12-17 23:44:42 -0300

    feat: Criando a controller de missas.

--- EscalaMensal.API/Controllers/MissasController.cs
+++ EscalaMensal.API/Controllers/MissasController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EscalaMensal.Domain.Entities;
+using EscalaMensal.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EscalaMensal.API.Controllers
 {
@@ -6,6 +8,39 @@ namespace EscalaMensal.API.Controllers
     [Route("api/[controller]")]
     public class MissasController : Controller
     {
-        
+        private readonly IMissasService _missasService;
+        public MissasController(IMissasService missasService)
+        {
+            _missasService = missasService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Missas>>> ObterPorEscalaId(int id)
+        {
+            return Ok(await _missasService.ObterPorEscalaIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Adicionar(Missas missa)
+        {
+            await _missasService.AdicionarAsync(missa);
+            return CreatedAtAction(nameof(ObterPorEscalaId), new { id = missa.EscalaId }, missa);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Atualizar(int id, Missas missa)
+        {
+            if (id != missa.Id)
+                return BadRequest("ID do parâmetro não confere com o da entidade");
+            await _missasService.AtualizarAsync(missa);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Remover(int id)
+        {
+            await _missasService.RemoverAsync(id);
+            return NoContent();
+        }
     }
 }
