ccaca45b3577efcad1e2a7218c27d8a6953719f1
João Vitor <joaovitor.oliveira1912@gmail.com>
2026-01-06 21:46:36 -0300

    feat: Validando a restrição e otimizando o código.

--- EscalaMensal.Infrastructure/Services/ItemMissaService.cs
+++ EscalaMensal.Infrastructure/Services/ItemMissaService.cs
@@ -15,13 +15,17 @@ namespace EscalaMensal.Application.Services
         private readonly IFuncaoRepository _funcaoRepository;
         private readonly IUsuarioRepository _usuarioRepository;
         private readonly IEscalaRepository _escalaRepository;
+        private readonly IMissasRepository _missaRepository;
+        private readonly IRestricaoRepository _restricaoRepository;
 
-        public ItemMissaService(IItemMissaRepository itemMissaRepository, IFuncaoRepository funcaoRepository, IUsuarioRepository usuarioRepository, IEscalaRepository escalaRepository)
+        public ItemMissaService(IItemMissaRepository itemMissaRepository, IFuncaoRepository funcaoRepository, IUsuarioRepository usuarioRepository, IEscalaRepository escalaRepository, IMissasRepository missaRepository, IRestricaoRepository restricaoRepository)
         {
             _itemMissaRepository = itemMissaRepository;
             _funcaoRepository = funcaoRepository;
             _usuarioRepository = usuarioRepository;
             _escalaRepository = escalaRepository;
+            _missaRepository = missaRepository;
+            _restricaoRepository = restricaoRepository;
         }
 
         public async Task<List<ItemMissa>> ObterPorMissaIdAsync(int escalaId)
@@ -31,89 +35,115 @@ namespace EscalaMensal.Application.Services
 
         public async Task AdicionarAsync(ItemMissa item)
         {
-            var funcao = await _funcaoRepository.ObterPorIdAsync(item.FuncaoId);
+            var funcao = await _funcaoRepository.ObterPorIdAsync(item.FuncaoId)
+                         ?? throw new Exception("Função não encontrada.");
 
-            if (funcao == null) throw new Exception("Função não encontrada.");
+            Usuario? usuario = null;
 
-            var usuario = item.UsuarioId.HasValue
-                          ? await _usuarioRepository.ObterPorIdAsync(item.UsuarioId.Value)
-                          : null;
-
-            if (usuario != null)
+            if (item.UsuarioId.HasValue)
             {
-                bool jaEscalado = await _itemMissaRepository.ExisteUsuarioNaMissaAsync(item.MissaId, usuario.Id);
-                if (jaEscalado)
+                usuario = await _usuarioRepository.ObterPorIdAsync(item.UsuarioId.Value);
+
+                if (usuario != null)
                 {
-                    throw new DomainException($"O usuário '{usuario.Nome}' já está escalado para outra função nesta missa.");
+                    if (await _itemMissaRepository.ExisteUsuarioNaMissaAsync(item.MissaId, usuario.Id))
+                    {
+                        throw new DomainException(
+                            $"O usuário '{usuario.Nome}' já está escalado para outra função nesta missa."
+                        );
+                    }
                 }
             }
 
-            string? erro = (funcao.Id, usuario) switch
+            var missa = await _missaRepository.ObterPorMissaIdAsync(item.MissaId);
+
+            if (usuario != null)
             {
-                (_, null) when funcao.Obrigatoria => $"A função '{funcao.Nome}' é obrigatória.",
+                var restricoes = await _restricaoRepository
+                    .ObterPorUsuarioIdAsync(usuario.Id, missa.Dia.Month, missa.Dia.Year);
 
-                (_, null) => null,
-
-                (_, var u) when u.Cargo < funcao.Cargo => $"Essa pessoa '{u.Nome}' não tem é um {funcao.Cargo} para servir nessa função.",
-
-                (1, var u) when u.Cargo == CargoEnum.Coroinha => "Coroinhas não podem assumir essa função.",
-
-                (6, var u) when u.Cargo == CargoEnum.Coroinha && u.Nivel < NivelEnum.Nivel2
-                     => "Coroinhas precisam ser Nível 2 para esta função.",
-
-                _ => null
-            };
-
-            if (erro != null)
-            {
-                throw new DomainException(erro);
+                if (restricoes?.Count > 0)
+                {
+                    throw new DomainException(
+                        $"O usuário '{usuario.Nome}' possui restrição para o dia {missa.Dia:dd/MM/yyyy}."
+                    );
+                }
             }
+
+            ValidarFuncaoUsuario(funcao, usuario);
 
             await _itemMissaRepository.AdicionarAsync(item);
         }
 
         public async Task AtualizarAsync(ItemMissa item)
         {
-            var funcao = await _funcaoRepository.ObterPorIdAsync(item.FuncaoId);
+            var funcao = await _funcaoRepository.ObterPorIdAsync(item.FuncaoId)
+                         ?? throw new Exception("Função não encontrada.");
 
-            if (funcao == null) throw new Exception("Função não encontrada.");
+            Usuario? usuario = null;
 
-            var usuario = item.UsuarioId.HasValue
-                          ? await _usuarioRepository.ObterPorIdAsync(item.UsuarioId.Value)
-                          : null;
-
-            if (usuario != null)
+            if (item.UsuarioId.HasValue)
             {
-                bool jaEscalado = await _itemMissaRepository.ExisteUsuarioNaMissaAsync(item.MissaId, usuario.Id);
-                if (jaEscalado)
+                usuario = await _usuarioRepository.ObterPorIdAsync(item.UsuarioId.Value);
+
+                if (usuario != null)
                 {
-                    throw new DomainException($"O usuário '{usuario.Nome}' já está escalado para outra função nesta missa.");
+                    if (await _itemMissaRepository.ExisteUsuarioNaMissaAsync(item.MissaId, usuario.Id))
+                    {
+                        throw new DomainException(
+                            $"O usuário '{usuario.Nome}' já está escalado para outra função nesta missa."
+                        );
+                    }
                 }
             }
 
+            var missa = await _missaRepository.ObterPorMissaIdAsync(item.MissaId);
+
+            if (usuario != null)
+            {
+                var restricoes = await _restricaoRepository
+                    .ObterPorUsuarioIdAsync(usuario.Id, missa.Dia.Month, missa.Dia.Year);
+
+                if (restricoes?.Count > 0)
+                {
+                    throw new DomainException(
+                        $"O usuário '{usuario.Nome}' possui restrição para o dia {missa.Dia:dd/MM/yyyy}."
+                    );
+                }
+            }
+
+            ValidarFuncaoUsuario(funcao, usuario);
+
+            await _itemMissaRepository.AtualizarAsync(item);
+        }
+
+        private static void ValidarFuncaoUsuario(Funcao funcao, Usuario? usuario)
+        {
             string? erro = (funcao.Id, usuario) switch
             {
-                (_, null) when funcao.Obrigatoria => $"A função '{funcao.Nome}' é obrigatória.",
+                (_, null) when funcao.Obrigatoria
+                    => $"A função '{funcao.Nome}' é obrigatória.",
 
-                (_, null) => null,
+                (_, null)
+                    => null,
 
-                (_, var u) when u.Cargo < funcao.Cargo => $"Essa pessoa '{u.Nome}' não tem é um {funcao.Cargo} para servir nessa função.",
+                (_, var u) when u.Cargo < funcao.Cargo
+                    => $"Essa pessoa '{u.Nome}' não é um {funcao.Cargo} para servir nessa função.",
 
-                (1, var u) when u.Cargo == CargoEnum.Coroinha => "Coroinhas não podem assumir essa função.",
+                (1, var u) when u.Cargo == CargoEnum.Coroinha
+                    => "Coroinhas não podem assumir essa função.",
 
                 (6, var u) when u.Cargo == CargoEnum.Coroinha && u.Nivel < NivelEnum.Nivel2
-                     => "Coroinhas precisam ser Nível 2 para esta função.",
+                    => "Coroinhas precisam ser Nível 2 para esta função.",
 
                 _ => null
             };
 
             if (erro != null)
-            {
                 throw new DomainException(erro);
-            }
-
-            await _itemMissaRepository.AtualizarAsync(item);
         }
+
+
 
         public async Task RemoverAsync(int id)
         {
