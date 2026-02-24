75daae13eaf833d0f23664b9cf66aa088581643d
João Vitor <joaovitor.oliveira1912@gmail.com>
2026-02-23 21:29:16 -0300

    feat: Utilizando DTO de Cargo Nivel Função Permitida

---
+++ EscalaMensal.Application/DTOs/CargoNivel/CargoNivelDto.cs
@@ -0,0 +1,17 @@
+﻿using EscalaMensal.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Application.DTOs.CargoNivel
+{
+    public class CargoNivelDto
+    {
+        public int Id { get; set; }
+        public CargoEnum Cargo { get; set; }
+        public NivelEnum Nivel { get; set; }
+        public int FuncaoId { get; set; }
+    }
+}
