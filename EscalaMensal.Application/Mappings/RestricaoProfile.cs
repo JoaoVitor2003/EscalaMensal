682103bb2983f3cba2667af2828100422d185630
João Vitor <joaovitor.oliveira1912@gmail.com>
2026-05-25 22:20:19 -0300

    feat: Criando o perfil do mapeamento entre entidade e DTO de Restrição

---
+++ EscalaMensal.Application/Mappings/RestricaoProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using EscalaMensal.Application.DTOs.Escala;
+using EscalaMensal.Application.DTOs.Restricao;
+using EscalaMensal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaMensal.Application.Mappings
+{
+    public class RestricaoProfile : Profile
+    {
+        public RestricaoProfile()
+        {
+            CreateMap<Restricao, RestricaoDto>();
+            CreateMap<RestricaoAdicionarDto, Restricao>();
+            CreateMap<RestricaoAtualizarDto, Restricao>();
+            CreateMap<EscalaDeleteDto, Escala>();
+        }
+    }
+}
